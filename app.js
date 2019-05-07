f8d07ae8ea979d1143521bbabe9c42a96536e44e
jm macapagal <jmdmacapagal@gmail.com>
2019-05-08 00:10:36 +0800

    added renderTodo feature

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ const todos = [
     'Sleep'
 ]
 
+renderTodo(todos)
+
+function renderTodo(todos) {
+    const todoDisplay = document.querySelector('#todoDisplay')
+    todoDisplay.innerHTML = ''
+    
+    todos.forEach(function(todo) {
+        todoDisplay.appendChild(renderTodoList(todo))
+        console.log(todo)
+    })
+    
+}
+
+function renderTodoList(todo) {
+    const ul = document.createElement('ul')
+    const li = document.createElement('li')
+    const a = document.createElement('a')
+    ul.appendChild(li)
+    a.textContent = todo
+    li.appendChild(a)
+    return ul
+}
+
 document.querySelector('#addForm').addEventListener('submit', function(e) {
     e.preventDefault()
     todos.push(e.target.addTodo.value)
+    renderTodo(todos)
 })
